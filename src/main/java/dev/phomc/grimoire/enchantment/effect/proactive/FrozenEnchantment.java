83d0de919fd303147f223404bcb3811ec8c60e53
anhcraft <huynhduyanh123123@gmail.com>
2023-03-14 16:25:19 +0700

    Rebalance enchantments 3

--- src/main/java/dev/phomc/grimoire/enchantment/effect/proactive/FrozenEnchantment.java
+++ src/main/java/dev/phomc/grimoire/enchantment/effect/proactive/FrozenEnchantment.java
@@ -7,9 +7,9 @@ import net.minecraft.world.effect.MobEffects;
 public class FrozenEnchantment extends ProactiveEffectEnchantment {
     public FrozenEnchantment(ResourceLocation identifier) {
         super(identifier, MobEffects.MOVEMENT_SLOWDOWN,
-                new int[]{0, 1, 2, 3, 4, 5},
-                new int[]{40, 60, 80, 120, 120, 150},
-                new float[]{0.1f, 0.2f, 0.2f, 0.2f, 0.3f, 0.3f}
+                new int[]{0, 1, 2, 3, 4},
+                new int[]{40, 60, 80, 120, 120},
+                new float[]{0.2f, 0.2f, 0.3f, 0.3f, 0.4f}
         );
     }
 }
--- src/main/java/dev/phomc/grimoire/enchantment/effect/proactive/PoisonEnchantment.java
+++ src/main/java/dev/phomc/grimoire/enchantment/effect/proactive/PoisonEnchantment.java
@@ -8,8 +8,8 @@ public class PoisonEnchantment extends ProactiveEffectEnchantment {
     public PoisonEnchantment(ResourceLocation identifier) {
         super(identifier, MobEffects.POISON,
                 new int[]{0, 1, 2, 3, 4},
-                new int[]{50, 50, 50, 80, 100},
-                new float[]{0.1f, 0.2f, 0.25f, 0.3f, 0.35f}
+                new int[]{60, 60, 80, 100, 100},
+                new float[]{0.2f, 0.3f, 0.4f, 0.4f, 0.5f}
         );
     }
 }
