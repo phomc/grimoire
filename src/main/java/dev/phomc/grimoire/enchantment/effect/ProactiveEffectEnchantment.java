210890e63604bf09410dd67c2aeab9a1f62be728
anhcraft <huynhduyanh123123@gmail.com>
2023-03-24 23:01:31 +0700

    Refactor effect enchantments

--- src/main/java/dev/phomc/grimoire/enchantment/effect/ProactiveEffectEnchantment.java
+++ src/main/java/dev/phomc/grimoire/enchantment/effect/ProactiveEffectEnchantment.java
@@ -1,47 +1,20 @@
 package dev.phomc.grimoire.enchantment.effect;
 
-import dev.phomc.grimoire.enchantment.attack.AbstractAttackEnchantment;
+import dev.phomc.grimoire.enchantment.EnchantmentTarget;
 import dev.phomc.grimoire.event.AttackRecord;
 import dev.phomc.grimoire.utils.DevModeUtils;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.enchantment.Enchantment;
 
-import java.util.concurrent.ThreadLocalRandom;
-
-public class ProactiveEffectEnchantment extends AbstractAttackEnchantment {
-    private final MobEffect effect;
-    private final int[] amplifiers;
-    private final float[] chances;
-    private final int[] duration;
-
-    public ProactiveEffectEnchantment(ResourceLocation identifier, MobEffect effect, int[] amplifiers, int[] duration, float[] chances) {
-        super(identifier, Enchantment.Rarity.COMMON);
-        this.effect = effect;
-        this.amplifiers = amplifiers;
-        if (amplifiers.length == 0) throw new IllegalArgumentException();
-        this.duration = duration;
-        if (amplifiers.length != duration.length) {
-            throw new IllegalArgumentException(String.format("%s: len(amplifiers) != len(duration)", identifier));
-        }
-        this.chances = chances;
-        if (amplifiers.length != chances.length) {
-            throw new IllegalArgumentException(String.format("%s: len(amplifiers) != len(chances)", identifier));
-        }
-        DevModeUtils.onEffectEnchantInit(identifier, "proactive", effect, amplifiers, duration, chances);
+public class ProactiveEffectEnchantment extends EffectEnchantment {
+    public ProactiveEffectEnchantment(ResourceLocation identifier, MobEffect effect, EffectStage... effectStages) {
+        super(identifier, Enchantment.Rarity.COMMON, EnchantmentTarget.MELEE.or(EnchantmentTarget.RANGED), effect, effectStages);
+        DevModeUtils.onEffectEnchantInit(identifier, "proactive", effect, effectStages);
     }
 
     @Override
-    public int getMaxLevel() {
-        return amplifiers.length;
-    }
-
-    @Override
-    protected void execute(AttackRecord attackRecord, int level) {
-        int index = clampLevel(level) - 1;
-        float rand = ThreadLocalRandom.current().nextFloat();
-        if (rand > chances[index]) return;
-        attackRecord.victim().addEffect(new MobEffectInstance(effect, duration[index], amplifiers[index]));
+    public void onAttack(AttackRecord attackRecord, int enchantLevel) {
+        execute(attackRecord.attacker(), enchantLevel);
     }
 }
--- src/main/java/dev/phomc/grimoire/enchantment/effect/passive/PetrifiedEnchantment.java
+++ src/main/java/dev/phomc/grimoire/enchantment/effect/passive/PetrifiedEnchantment.java
@@ -1,5 +1,6 @@
 package dev.phomc.grimoire.enchantment.effect.passive;
 
+import dev.phomc.grimoire.enchantment.effect.EffectStage;
 import dev.phomc.grimoire.enchantment.effect.PassiveEffectEnchantment;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffects;
@@ -7,9 +8,11 @@ import net.minecraft.world.effect.MobEffects;
 public class PetrifiedEnchantment extends PassiveEffectEnchantment {
     public PetrifiedEnchantment(ResourceLocation identifier) {
         super(identifier, MobEffects.MOVEMENT_SLOWDOWN,
-                new int[]{1, 2, 3, 4, 5},
-                new int[]{60, 80, 120, 120, 150},
-                new float[]{0.2f, 0.3f, 0.3f, 0.4f, 0.5f}
+                new EffectStage(1, 60, 0.2),
+                new EffectStage(2, 80, 0.3),
+                new EffectStage(3, 120, 0.3),
+                new EffectStage(4, 120, 0.4),
+                new EffectStage(5, 150, 0.5)
         );
     }
 }
